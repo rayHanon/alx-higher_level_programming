f47909a3d86c0940a16ba4ddebf13e6991e322cc
rayHanon <rayhanon04@gmail.com>
2024-06-07 05:50:11 -0700

    Python concatinating

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-result = str[39:65] + " with Python"
-
-print(result)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:66], end=' ')
+print(str[:6])
