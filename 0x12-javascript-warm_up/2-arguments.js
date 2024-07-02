1067017920d27e99dcb513ffcbc646d058d290cc
rayHanon <rayhanon04@gmail.com>
2024-07-02 12:38:10 +0300

    Passing arguments in JS

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const arg = process.argv.slice(2);
 if (arg.length === 0) {
-  console.log('No argument')
-}  else if (arg.length === 1) {
+  console.log('No argument');
+} else if (arg.length === 1) {
   console.log('Argument found');
-}  else {
+} else {
   console.log('Arguments found');
 }
