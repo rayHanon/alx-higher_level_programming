afd1b81578b0aec43a460c946665c2ba9667c490
rayHanon <rayhanon04@gmail.com>
2024-07-05 20:43:26 +0300

    Only numbers

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log('Not a number');
+}  else {
+  const number = Number(args[0]);
+ if (Number.isInteger(number)) {
+  console.log(`My number: ${number}`);
+ } else {
+  console.log('Not a number');
+ }
+}
